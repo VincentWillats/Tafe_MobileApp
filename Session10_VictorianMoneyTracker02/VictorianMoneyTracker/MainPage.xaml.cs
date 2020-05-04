8c7e86b04c7c9a0be259b7e385dac3930eff1233
VincentWillats <vincentwillats.software@gmail.com>
2020-05-04 09:44:03 +0800

    Started Session10

---
+++ Session10_VictorianMoneyTracker02/VictorianMoneyTracker/MainPage.xaml.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+/**
+ * Victorian Money Tracker View Controller
+ * 
+ * Has two event handles, one for Convert clicks and one for adding/subtracting currency
+ * 
+ * Has an UpdateUI function which is called in all event handlers and in mainpage constructor
+ * UpdateUI updates all labels/text and visability of buttons
+ **/
+
+namespace VictorianMoneyTracker
+{
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        currency_Model Currency = new currency_Model();
+
+        public MainPage()
+        {
+            InitializeComponent();
+            UpdateUI();
+        }
+
+        private void Currency_Convert_Pressed(object sender, EventArgs e)
+        {
+            ImageButton pressedButton = (ImageButton)sender;
+            string pressedButtonName = pressedButton.ClassId;
+
+            switch (pressedButtonName)
+            {
+                // Pound
+                case "convertDownPoundID":
+                    Currency.ConvertPoundDown();
+                    break;           
+
+                // Crown
+                case "convertDownCrownID":
+                    Currency.ConvertCrownDown();
+                    break;
+                case "convertUpCrownID":
+                    Currency.ConvertCrownUp();
+                    break;
+
+                // Shilling
+                case "convertDownShillingID":
+                    Currency.ConvertShillingDown();
+                    break;
+                case "convertUpShillingID":
+                    Currency.ConvertShillingUp();
+                    break;
+
+                // Pence
+                case "convertDownPenceID":
+                    Currency.ConvertPenceDown();
+                    break;
+                case "convertUpPenceID":
+                    Currency.ConvertPenceUp();
+                    break;
+
+                // Farthing       
+                case "convertUpFarthingID":
+                    Currency.ConvertFarthingUp();
+                    break;
+            }
+
+            UpdateUI();
+        }
+
+        private void Currency_Adjust_Pressed(object sender, EventArgs e)
+        {
+            Button pressedButton = (Button)sender;
+            string pressedButtonName = pressedButton.ClassId;           
+
+            switch (pressedButtonName)
+            {
+                // Pound
+                case "lessPoundID":
+                    Currency.Pounds--;
+                    break;
+                case "morePoundID":
+                    Currency.Pounds++;
+                    break;
+
+                // Crown
+                case "lessCrownID":
+                    Currency.Crowns--;
+                    break;
+                case "moreCrownID":
+                    Currency.Crowns++;
+                    break;
+
+                // Shilling
+                case "lessShillingID":
+                    Currency.Shillings--;
+                    break;
+                case "moreShillingID":
+                    Currency.Shillings++;
+                    break;
+
+                // Pence
+                case "lessPenceID":
+                    Currency.Pence--;
+                    break;
+                case "morePenceID":
+                    Currency.Pence++;
+                    break;
+
+                // Farthing
+                case "lessFarthingID":
+                    Currency.Farthings--;
+                    break;
+                case "moreFarthingID":
+                    Currency.Farthings++;
+                    break;
+            }
+            UpdateUI();
+        }
+
+        private void UpdateUI()
+        {
+            totalPoundText.Text = "£" + Currency.Pounds.ToString();
+            totalCrownText.Text = Currency.Crowns.ToString() + "c";
+            totalShillingText.Text = Currency.Shillings.ToString() + "s";
+            totalPenceText.Text = Currency.Pence.ToString() + "d";
+            totalFarthingText.Text = Currency.Farthings.ToString() + "f";
+
+            poundText.Text = Currency.Pounds.ToString();
+            crownText.Text = Currency.Crowns.ToString();
+            shillingText.Text = Currency.Shillings.ToString();
+            penceText.Text = Currency.Pence.ToString();
+            farthingText.Text = Currency.Farthings.ToString();
+            
+            _ = Currency.Pounds == 1 ? poundLabel.Text = "pound" : poundLabel.Text = "pounds"; // Changes singlar/plural 
+            _ = Currency.Pounds == 0 ? convertDownPound.IsVisible = false : convertDownPound.IsVisible = true; // Sets button visability
+                         
+            _ = Currency.Crowns == 1 ? crownLabel.Text = "crown" : crownLabel.Text = "crowns"; // Changes singlar/plural 
+            _ = Currency.Crowns == 0 ? convertDownCrown.IsVisible = false : convertDownCrown.IsVisible = true; // Sets button visability
+            _ = Currency.Crowns < 4 ? convertUpCrown.IsVisible = false : convertUpCrown.IsVisible = true; // Sets button visability
+
+            _ = Currency.Shillings == 1 ? shillingLabel.Text = "shilling" : shillingLabel.Text = "shillings"; // Changes singlar/plural 
+            _ = Currency.Shillings == 0 ? convertDownShilling.IsVisible = false : convertDownShilling.IsVisible = true; // Sets button visability
+            _ = Currency.Shillings < 5 ? convertUpShilling.IsVisible = false : convertUpShilling.IsVisible = true;  // Sets button visability
+
+            _ = Currency.Pence == 1 ? penceLabel.Text = "penny" : penceLabel.Text = "pence"; // Changes singlar/plural 
+            _ = Currency.Pence == 0 ? convertDownPence.IsVisible = false : convertDownPence.IsVisible = true;   // Sets button visability
+            _ = Currency.Pence < 12 ? convertUpPence.IsVisible = false : convertUpPence.IsVisible = true;   // Sets button visability
+
+            _ = Currency.Farthings == 1 ? farthingLabel.Text = "farthing" : farthingLabel.Text = "farthings";  // Changes singlar/plural           
+            _ = Currency.Farthings < 4 ? convertUpFarthing.IsVisible = false : convertUpFarthing.IsVisible = true;   // Sets button visability        
+        }
+
+
+    }
+}
+
+
---
+++ Session10_VictorianMoneyTracker02/VictorianMoneyTracker/currency_Model.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+/**
+ * 
+ * Currency Data Model
+ * Holds current currency and functions get/set and to convert between.
+ * 
+ * Data validation to stop currency going negative held in set functions
+ *  
+ **/
+
+
+
+namespace VictorianMoneyTracker
+{
+    class currency_Model
+    {
+        private int _pounds;
+        public int Pounds
+        {
+            get { return _pounds; }
+            set { _pounds = value;
+                if (!(_pounds >= 0)) 
+                { _pounds = 0; }
+            }
+        }
+
+        private int _crowns;
+        public int Crowns
+        {
+            get { return _crowns; }
+            set { _crowns = value;
+                if (!(_crowns >= 0))
+                { _crowns = 0; }
+            }
+        }
+
+        private int _shillings;
+        public int Shillings
+        {
+            get { return _shillings; }
+            set { _shillings = value;
+                if (!(_shillings >= 0))
+                { _shillings = 0; }
+            }
+        }
+
+        private int _pence;
+        public int Pence
+        {
+            get { return _pence; }
+            set { _pence = value;
+                if (!(_pence >= 0))
+                { _pence = 0; }
+            }
+        }
+
+        private int _farthings;
+        public int Farthings
+        {
+            get { return _farthings; }
+            set { _farthings = value;
+                if (!(_farthings >= 0))
+                { _farthings = 0; }
+            }
+        }
+
+        public void ConvertPoundDown()
+        {
+            if (_pounds >= 1)
+            {
+                _pounds--;
+                _crowns = _crowns + 4;
+            }        
+            else
+            {
+                Console.WriteLine("Not enough pounds to convert down to crowns");                
+            }
+        }
+
+        public void ConvertCrownUp()
+        {
+        
+            if (_crowns >= 4)
+            {
+                _crowns = _crowns - 4;
+                _pounds++;                
+            }
+            else
+            {
+                Console.WriteLine("Not enough crowns to convert up to pounds");
+            }
+        }
+
+        public void ConvertCrownDown()
+        {
+            if (_crowns >= 1)
+            {
+                _crowns--;
+                _shillings = _shillings + 5;                
+            }
+            else
+            {
+                Console.WriteLine("Not enough crowns to convert down to shillings");
+            }
+        }
+
+        public void ConvertShillingUp()
+        {
+            if (_shillings >= 5)
+            {
+                _shillings = _shillings - 5;
+                _crowns++;
+            }
+            else
+            {
+                Console.WriteLine("Not enough shillings to convert up to crowns");
+            }
+        }
+
+        public void ConvertShillingDown()
+        {
+            if (_shillings >= 1)
+            {
+                _shillings--;
+                _pence = _pence + 12;
+            }
+            else
+            {
+                Console.WriteLine("Not enough shillings to convert down to pence");
+            }
+        }
+
+        public void ConvertPenceUp()
+        {
+            if (_pence >= 12)
+            {
+                _pence = _pence - 12;
+                _shillings++;
+            }
+            else
+            {
+                Console.WriteLine("Not enough pence to convert up to shillings");
+            }
+        }
+
+        public void ConvertPenceDown()
+        {
+            if (_pence >= 1)
+            {
+                _pence--;
+                _farthings = _farthings + 4;
+            }
+            else
+            {
+                Console.WriteLine("Not enough pence to convert down to farthing");
+            }
+        }
+
+        public void ConvertFarthingUp()
+        {
+            if (_farthings >= 4)
+            {
+                _farthings = _farthings - 4;
+                _pence++;
+            }
+            else
+            {
+                Console.WriteLine("Not enough farthings to convert up to pences");
+            }
+        }
+    }
+}
